959a2a5f3dd262e0a08eb6535fa8919b32f2e654
Simon <warcraft.simon@gmail.com>
2020-05-14 10:18:07 +0200

    new functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,34 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import {User} from './user';
 
 admin.initializeApp();
 
-exports.createUserWhen1stLogin = functions.auth
-  .user().onCreate(((user, context) => {
+exports.createUserAndCartWhen1stLogin = functions.auth
+  .user().onCreate(((user) => {
+    const key = admin.database().ref().push().key
+    return admin.firestore().doc('shopping-carts/' + key).set({
+      productsInCart: []
+    }).then(() => {
       return admin.firestore().doc('users/' + user.uid).set({
         uid: user.uid,
         email: user.email,
         name: user.displayName,
         picUrl: 'https://i.imgur.com/YcP0tikb.jpg',
-        isAdmin: false
+        isAdmin: false,
+        cartId: key
       })
+    })
+
     }
   ));
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.deleteDateConnected2UserWhenUserDelete = functions.firestore.document('users/{id}')
+  .onDelete((snapshot, context) => {
+    const user = snapshot.data() as User
+    return admin.auth().deleteUser(context.params.id).then(() => {
+      return admin.firestore().doc('shopping-carts/' + user.cartId).delete()
+    })
+  })
+
+
---
+++ functions/src/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  email: string;
+  name: string;
+  uid: string;
+  picUrl?: string;
+  isAdmin: boolean;
+  cartId: string;
+}
